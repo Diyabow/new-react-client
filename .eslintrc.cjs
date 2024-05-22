21f0c483f50e81dc0f5321d02c7ca0db734a0e96
ManvikaDivya <divyanm.22mca@jyotinivas.org>
2024-05-22 15:15:16 +0530

    test message

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,10 +12,12 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "semi": [2, "always"],
+    "indent": ["error", 2],
   },
 }
+
